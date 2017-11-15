b304bf488a3a0f1596bab0bde83d02e1178c4142
ceoimon <ceoimonzyu@gmail.com>
2017-11-15 19:54:34 +0800

    Fix tests by handling revert error

--- test/utils.js
+++ test/utils.js
@@ -4,9 +4,10 @@ module.exports = {
       await promise
     } catch (error) {
       const invalidJump = error.message.search('invalid JUMP') >= 0
+      const revert = error.message.search('revert') >= 0
       const invalidOpcode = error.message.search('invalid opcode') >= 0
       const outOfGas = error.message.search('out of gas') >= 0
-      assert(invalidJump || invalidOpcode || outOfGas, `Expected throw, got '${error}' instead`)
+      assert(invalidJump || invalidOpcode || outOfGas || revert, `Expected throw, got '${error}' instead`)
       return
     }
     assert.fail('Expected throw not received')
