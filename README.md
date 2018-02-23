bfc3c2860d312c3917e7bb76a8c1680b44dc06f3
ceoimon <ceoimonzyu@gmail.com>
2018-02-23 10:18:16 +0800

    Remove unnecessary stuff

--- README.md
+++ README.md
@@ -40,37 +40,8 @@ yarn migrate
 #### Using [infura.io](https://infura.io/) and Mnemonic
 
 ```bash
-yarn migrate --network rinkeby --mnemonic 'foo bar baz quz ...'
-# or --network mainnet
+# Use TRUSTMESH_MNEMONIC and TRUSTMESH_ETH_SENDER to config
+TRUSTMESH_MNEMONIC='foo bar baz quz...' \
+TRUSTMESH_ETH_SENDER='0x123...' yarn migrate --network rinkeby
 ```
-
-<details>
-  <summary>Advanced Deployment Configurations</summary>
-
-  #### Command Line Options
-
-  All options of `truffle migrate` (http://truffleframework.com/docs/advanced/commands#migrate) are supported.
-
-  ```
-    yarn migrate [options]
-
-    Options:
-
-      -h, --host       Ethereum node URL hostname
-      -p, --port       Ethereum node URL port
-      -m, --mnemonic   BIP32 HD wallet passphrase (Wallet client's passphrase, MetaMask's seed phrase)
-      -i, --index      HD wallet index
-      --useHttps       Use `https` instead of `http`
-      --gas            Gas limit
-      --gasPrice       Gas price
-      --from           Address used during migrations
-      
-  ```
-
-  #### Using `deploy.config.json`
-
-  ```bash
-  cp _deploy.config.json deploy.config.json
-  ```
-</details>
 
--- truffle.js
+++ truffle.js
@@ -1,85 +1,16 @@
-const yargs = require('yargs')
-const fs = require('fs')
 const HDWalletProvider = require('truffle-hdwallet-provider')
-
-let configs
-try {
-  // try to load config file
-  configs = require('./deploy.config.json')
-} catch (err) {
-  // default configurations
-  configs = {
-    port: 8545,
-    host: 'localhost',
-    wallet: {
-      mnemonic: '',
-      index: 0
-    }
-  }
-}
-
-// command line arguments
-const argv = yargs.argv
-// aliases
-Object.assign(argv, {
-  port: argv.p,
-  host: argv.h,
-  mnemonic: argv.m,
-  index: argv.i
-})
-
-function getDeployConfigs(options) {
-  const { 
-    networkID = '*',
-    host = configs.host,
-    port: configPort = configs.port,
-    mnemonic = configs.wallet.mnemonic,
-    index = configs.wallet.index,
-    gas = configs.gas,
-    gasPrice = configs.gasPrice,
-    from = configs.from,
-    useHttps = configs.useHttps,
-  } = Object.assign({}, argv, options)
-
-  const port = typeof configPort !== 'undefined'
-    ? Number(configPort)
-    : Boolean(useHttps) ? 443 : 80
-
-  const provider = typeof host !== 'undefined' && mnemonic !== ''
-    ? new HDWalletProvider(
-        mnemonic,
-        `${Boolean(useHttps) ? 'https' : 'http'}://${host}:${port}`,
-        Number(index)
-      )
-    : undefined
-
-  return {
-    host: provider ? undefined : host,
-    port: provider ? undefined : port,
-    network_id: networkID,
-    gas,
-    gasPrice,
-    from: from !== '' ? from : undefined,
-    provider
-  }
-}
 
 module.exports = {
   networks: {
-    // default network
-    development: getDeployConfigs(),
-    // specified by --network
-    mainnet: getDeployConfigs({
-      host: 'mainnet.infura.io',
-      useHttps: true,
-      port: 443,
-      networkID: 1
-    }),
-    rinkeby: getDeployConfigs({
-      host: 'rinkeby.infura.io',
-      useHttps: true,
-      port: 443,
-      networkID: 4
-    })
+    development: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*' // Match any network id
+    },
+    rinkeby: {
+      provider: new HDWalletProvider(process.env.TRUSTMESH_MNEMONIC, 'https://rinkeby.infura.io', 0),
+      network_id: 4,
+      from: process.env.TRUSTMESH_ETH_SENDER
+    }
   }
 }
