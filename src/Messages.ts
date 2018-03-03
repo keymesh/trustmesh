c1468c523a44ea60be51b2015d19ddc1da1186cd
Howard Yeh <hayeah@gmail.com>
2018-03-03 12:00:48 +0800

    Revert "[WIP] Change APIs, use reasonable Promise instead of PromiEvent"

--- src/Messages.ts
+++ src/Messages.ts
@@ -1,32 +1,19 @@
-import { Tx, BlockType } from 'web3/types'
+import { Tx, PromiEvent, TransactionReceipt, BlockType } from 'web3/types'
 
 import { BaseContract } from './BaseContract'
 
 import * as info from '../build/contracts/Messages.json'
 
-import {
-  transactionPromiEventToPromise,
-  getProcessingTransactionHandlers,
-  IProcessingTransaction,
-} from './utils'
-
 export class Messages extends BaseContract {
   public static info: IDeployInfo = info
 
-  public async publish(message: string, options: Tx = {}): Promise<IProcessingTransaction> {
-    const transactionHash = await transactionPromiEventToPromise(
-      this.contract.methods.publish(message).send({ from: this.web3.eth.defaultAccount, ...options }),
-    )
-    const handlers = getProcessingTransactionHandlers(this.web3, transactionHash)
-
-    return {
-      transactionHash,
-      ...handlers,
-    }
+  public publish(message: string, options: Tx = {}): PromiEvent<TransactionReceipt> {
+    return this.contract.methods.publish(message)
+      .send({ from: this.web3.eth.defaultAccount, ...options })
   }
 
   public async getMessages(options: IGetMessagesOptions = {}): Promise<IQueriedMessages> {
-    return this.getEventsData<IMessage>('Publish', options)
+    return super.getEventsData<IMessage>('Publish', options)
   }
 }
 
